c43258e409561c73d4064f88881447e2c2ab3d22
Wesley-S-Alves <wesley.s.alves@outlook.com>
2022-06-05 16:33:10 -0300

    arrays

--- Back-End/Java/Arrays/Main.java
+++ Back-End/Java/Arrays/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int [] vetor = {8,5,9,-17,14,204};
-        //Ex1_OrdemInversa.inverteVetor(vetor);
-        //Ex2_ContadorConsoante.contaConsoante();
-        //NumerosAleatorios.randomNumbers();
+        Ex1_OrdemInversa.inverteVetor(vetor);
+        Ex2_ContadorConsoante.contaConsoante();
+        NumerosAleatorios.randomNumbers();
         MatrizMultidimensional.matrizMultidimensional();
     }
 }
