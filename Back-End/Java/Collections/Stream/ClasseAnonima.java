21879b2baed96522e24003d732d64cc80b331555
Wesley-S-Alves <wesley.s.alves@outlook.com>
2022-06-06 16:23:04 -0300

    Classe Anonima

---
+++ Back-End/Java/Collections/Stream/ClasseAnonima.java
@@ -0,0 +1,10 @@
+package Stream;
+/*
+Uma classe que não recebeu um nome e é declarada e instaciada
+em uma única instrução.
+seu uso deve ser considerado em casos em que alguma classe só será
+utilizada uma única vez
+ */
+
+public class ClasseAnonima {
+}
