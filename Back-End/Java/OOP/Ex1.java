dd0845d7a753d6a048da4b2cff0d340049a05edf
Wesley-S-Alves <wesley.s.alves@outlook.com>
2022-06-07 10:08:54 -0300

    ex1

---
+++ Back-End/Java/OOP/Ex1.java
@@ -0,0 +1,10 @@
+public class Ex1 {
+    public static void main(String[] args) {
+        Carro car1 = new Carro("Gol", "Wolkswagen","Prata","2012","15.6","18","Automático",5,58.7,0d );
+
+        System.out.println(car1.toString());
+    }
+
+
+}
+
